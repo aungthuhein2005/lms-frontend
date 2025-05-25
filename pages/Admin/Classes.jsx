4d84eb888bf2553276c047224baa725580ab155a
aungthuhein2005 <aungthuhein.ath05@gmail.com>
2025-05-25 21:53:10 +0630

    mrege with ymz

--- pages/Admin/Classes.jsx
+++ pages/Admin/Classes.jsx
@@ -3,42 +3,73 @@ import Table from 'react-bootstrap/Table';
 import { Button } from 'react-bootstrap';
 import { Modal } from "react-bootstrap";
 import { useState,useEffect } from 'react';
-import { useAddClassMutation, useDeleteClassMutation, useGetClassesQuery } from '../../features/api/classApiSlice';
 export default function Classes() {
+  const [classes, setClasses] = useState([]); 
   const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [eClass, setEClass] = useState({ id: "", name: "", description: "" });
+  const [editStatus, setEditStatus] = useState(false);
+ 
 
-  const {data, isLoading, error} = useGetClassesQuery();
-  const [addClass, { isLoading: isAddingClass }] = useAddClassMutation();
-  const [deletedClass] = useDeleteClassMutation();
-
-async function handleAddClass() {
-    try {
-      await addClass({ name, description }).unwrap();  // Call mutation function
+  async function addClass() {
+    await axios
+      .post("http://localhost:8383/class/create",{name,description})
+      .then((response) => setClasses(prev => [...prev,response.data]));
       setName('');
       setDescription('');
-      handleClose();
-      console.log('Class added successfully');
-    } catch (err) {
-      console.error('Failed to add class:', err);
-    }
   }
 
-  const handleDeleteClass = (id) => {
-    const result = confirm('Are you sure? You want to delete this class')
-    if (result) {
-      try {
-        deletedClass(id)
-        console.log('Class deleted successfully');
-      } catch (error) {
-        alert('Failed to delete class.')
-      }
-    }
+  useEffect(() => {
+    getClasses(); //data from localhost enters response
+  }, []);
+
+  async function getClasses() {
+    await axios
+      .get("http://localhost:8383/class/all")
+      .then((response) => setClasses(response.data));
+
   }
 
+  useEffect(() => {
+    getClasses(); //data from localhost enters response
+  }, []);
+
+  function editClass(editedClass) {
+    setEditStatus(true);
+    setEClass(editedClass);
+    console.log(editClass);
+  }
+  async function updateClass(updatedClass) {
+    await axios
+      .put(`http://localhost:8383/class/update/${updatedClass.id}`,updatedClass)
+      .then((response) => console.log(response.data));
+    setEditStatus(false);
+    setEClass({id:"", name: "", description: "",schedule:"" });
+    getClasses();
+  }
+
+  async function deletedClass(id) {
+    await axios
+    .delete(`http://localhost:8383/class/delete/${id}`)
+    .then((response)=>console.log(response.data));
+    getClasses();    
+  }
+
+  async function detailClass(id) {
+    await axios
+      .get(`http://localhost:8383/class/view/${id}`)
+      .then((response) => setClasses(response.data));
+
+  }
 
   return (
       <div>
@@ -77,14 +108,14 @@ async function handleAddClass() {
                 <Button variant="secondary" onClick={handleClose}>
                   Close
                 </Button>
-                <Button variant="primary" onClick={handleAddClass}>
+                <Button variant="primary" onClick={() => addClass()}>
                   Save Changes
                 </Button>
               </Modal.Footer>
             </Modal>      </div>
-      <h5>Total - {data?.length}</h5>
+      <h5>Total - </h5>
 
-    <Table striped bordered hover>
+      <Table striped bordered hover>
       <thead>
         <tr>
           <th>Class Code</th>
@@ -95,23 +126,62 @@ async function handleAddClass() {
         </tr>
       </thead>
       <tbody>
-        {data?.map((cls) =>(
-            <tr key={cls.id}>
-              <td>{cls.id}</td>
-              <td>{cls.name}</td>
-              <td>{cls.description}</td>
-              <td>{cls.schedule}</td>
+        {classes.map((classData) =>(
+          editStatus && classData.id == eClass.id ? (
+            <tr key={classData.id}>
+              <td>{classData.id}</td>
               <td>
-                    <Button className="btn btn-primary me-2" size="sm" onClick={()=> editedClass(cls)}><i className="bx bxs-edit-alt"></i></Button>
-                    <Button className="btn btn-success me-2" size="sm"><i className="bx bx-detail"></i></Button>
-                    <Button className="btn btn-danger me-2" size="sm" onClick={()=> handleDeleteClass(cls.id)}><i className="bx bxs-trash"></i></Button>
+              <input type='text' value={eClass.name} onChange={(e)=> setEClass({...eClass,name:e.target.value})} className='form-control'/>
               </td>
-        </tr>
-        ))}
-        
-      </tbody>
-    </Table>
-    </div>
-    
-  )
+              <td>
+                <input type='text' value={eClass.description} onChange={(e)=> setEClass({...eClass,description:e.target.value})} className='form-control'/>
+              </td>
+              <td>
+                  <button
+                    className="btn btn-warning me-2"
+                    onClick={() => updateClass(eClass)}
+                  >
+                    <i className="bx  bx-save"></i>
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deletedClass(classData.id)}
+                  >
+                    <i className="bx bxs-trash"></i>
+                  </button>
+                </td>
+              </tr>):
+              (
+              <tr key={classData.id}>
+              <td>{classData.id}</td>
+              <td>{classData.name}</td>
+              <td>{classData.description}</td>
+              <td>{classData.schedule}</td>
+              <td>
+                  <button
+                    className="btn btn-warning me-2"
+                    onClick={() => editClass(classData)}
+                  >
+                    <i className="bx bxs-edit-alt"></i>
+                  </button>
+                  <button
+                    className="btn btn-danger me-2"
+                    onClick={() => deletedClass(classData.id)}
+                  >
+                    <i className="bx bxs-trash"></i>
+                  </button>
+                  <button
+                    className="btn btn-success me-2"
+                    onClick={() => detailClass(classData.id)}
+                  >
+                   <i className="bx bx-detail"></i>
+                  </button>
+                </td>
+              </tr>
+              )
+            ))}
+          </tbody>
+          </Table>
+          </div>
+  )          
 }
